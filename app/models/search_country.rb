84f5389ffa20d0ed6fb829c093745a525bb37c83
Jason McBride <jsononrails@gmail.com>
2014-06-05 22:12:26 -0400

    fixed country search to be unique

--- app/models/search_country.rb
+++ app/models/search_country.rb
@@ -3,6 +3,7 @@ class SearchCountry < ActiveRecord::Base
   def self.countries_for(prefix)
     countries = Entry.where("lower(country) like ?", "#{prefix.downcase}_%")
     .select('country')
+    .distinct()
     .order("country asc")
     .limit(10).pluck(:country)
   end
