ef28a53c753c107862cee53bd57f81a6b623cf9e
Jason McBride <jsononrails@gmail.com>
2014-06-06 00:05:40 -0400

    expire headers set far future

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,14 @@ Rails.application.configure do
   # Puts ActionDispatch::Static in middleware stack which we are going to replace with
   # Rack::Zippy::AssetServer
   config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=#{1.year.to_i}"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
