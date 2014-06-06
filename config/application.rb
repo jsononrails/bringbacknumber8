f73c73b9aca381149e00409ed0e85d7888dc72b2
Jason McBride <jsononrails@gmail.com>
2014-06-05 23:51:25 -0400

    rack defaltor

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Bringbacknumber8
   class Application < Rails::Application
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
