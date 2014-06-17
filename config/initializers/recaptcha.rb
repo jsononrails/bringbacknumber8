e3fd27c04104b1a88f9e2c87d51e939c3d4bc64a
Jason McBride <jsononrails@gmail.com>
2014-06-16 23:01:42 -0400

    add facebook and twitter buttons

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = ENV['PUBLIC_CAPTCHA_KEY'].gsub(/\s+/, "")
-  config.private_key = ENV['PRIVATE_CAPTCHA_KEY'].gsub(/\s+/, "")
+  config.public_key  = ENV['PUBLIC_CAPTCHA_KEY'].to_s.gsub(/\s+/, "")
+  config.private_key = ENV['PRIVATE_CAPTCHA_KEY'].to_s.gsub(/\s+/, "")
 end
